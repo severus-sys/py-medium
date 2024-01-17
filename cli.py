35137178972e0a60321114a7ce0d31d74d8fa29a
severus-sys <talat.student@gmail.com>
2024-01-17 20:58:59 +0300

    fix type

--- cli.py
+++ cli.py
@@ -4,7 +4,7 @@ from predictor import DepthEstimationModel
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Depth estimation using ZoeDepth")
+    parser = argparse.ArgumentParser(description="Depth estimation using ZoeDepth.")
     parser.add_argument("input_image", help="Path to input image.")
     parser.add_argument("output_image", help="Path to output depth map.")
     args = parser.parse_args()
